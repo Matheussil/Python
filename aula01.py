12342cc4352ce0a3fcc93201f2a5096b73f8a2fe
Sammy <35262710+sammy08@users.noreply.github.com>
2018-10-10 11:49:39 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,7 @@
+valueOne = int(input("Valor da compra "))
+valueTwo = int(input("Valor da compra"))
+calc = valueOne + valueTwo
+if calc >= 100:
+	  print (f"Desconto recebido é {calc - 80}%")
+else:
+	print("sai daqui") 
